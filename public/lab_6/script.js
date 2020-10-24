b7a2ad53d4f5adc900dd16fafde4920f5db1f10f
cwu99 <70460558+cwu99@users.noreply.github.com>
2020-10-24 13:54:29 -0400

    update

--- public/lab_6/script.js
+++ public/lab_6/script.js
@@ -17,6 +17,12 @@ function sortFunction(a, b, key) {
   return 0;
 }
 
+function getRandomInt(min, max) {
+  min1 = Math.ceil(min);
+  max1 = Math.floor(max);
+  return Math.floor(Math.random() * (max1 - min1 + 1) + min1);
+}
+
 document.body.addEventListener('submit', async (e) => {
   e.preventDefault(); // this stops whatever the browser wanted to do itself.
   const form = $(e.target).serializeArray(); // here we're using jQuery to serialize the form
@@ -29,7 +35,25 @@ document.body.addEventListener('submit', async (e) => {
   })
     .then((fromServer) => fromServer.json())
     .then((fromServer) => {
-      // You're going to do your lab work in here. Replace this comment.
+      if (document.querySelector('.flex-inner')) {
+        document.querySelector('.flex-inner').remove();
+      }
+      result = range(10);
+      result = result.map(() => {
+        const num = getRandomInt(0, 243);
+        return fromServer[num];
+      });
+      result = result.sort((a, b) => sortFunction(b, a, 'name'));
+      const ol = document.createElement('ol');
+      ol.className = 'flex-inner';
+      $('form').prepend(ol);
+      result.forEach((el, i) => {
+        console.log(el.code);
+        const li = document.createElement('li');
+        $(li).append(`<input type="checkbox" value=${el.code} id=${el.code}/>`);
+        $(li).append(`<label for=${el.code}>${el.name} </label>`);
+        $(ol).append(li);
+      });
       console.log('fromServer', fromServer);
     })
     .catch((err) => console.log(err));
